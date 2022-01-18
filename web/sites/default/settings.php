6fa3e638419fdb3b5b12b270bce742bc17bbf8e8
Arghyajit Lodh <arghyajit.lodh@deptagency.com>
2022-01-18 17:15:03 +0530

    Initial drupal commit

---
+++ web/sites/default/settings.php
@@ -0,0 +1,35 @@
+<?php
+
+global $content_directories;
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+/**
+ * Skipping permissions hardening will make scaffolding
+ * work better, but will also raise a warning when you
+ * install Drupal.
+ *
+ * https://www.drupal.org/project/drupal/issues/3091285
+ */
+// $settings['skip_permissions_hardening'] = TRUE;
+
+/**
+ * If there is a local settings file, then include it
+ */
+$local_settings = __DIR__ . "/settings.local.php";
+if (file_exists($local_settings)) {
+  include $local_settings;
+}
+$settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config';
+
+/**
+ * "Trusted host settings" are not necessary on Pantheon; traffic will only
+ * be routed to your site if the host settings match a domain configured for
+ * your site in the dashboard.
+ */
+if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+    $settings['trusted_host_patterns'][] = '.*';
+}
