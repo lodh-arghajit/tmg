3a28209d8173ba41fe195d5cbf1081334fca8a1e
sumantra <sumantrakundu@gmail.com>
2022-03-04 10:41:30 +0530

    Theme Commeted

---
+++ web/themes/custom/tmw_theme/js/common.ui.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function(){
+    var hamburger = jQuery('.navbar-toggle'),
+        navBar = jQuery('.navigation'),
+        backDrop = '<div class="backdrop"></div>';
+        hamburger.on('click', function () {
+            navBar.addClass('push-menu-visible');
+			jQuery('body').addClass('show-nav');
+            jQuery(backDrop).appendTo('body').on('click', function () {
+                navBar.removeClass('push-menu-visible');
+                jQuery(this).remove();
+                jQuery('body').removeClass('show-nav');
+            });
+        });
+
+    var navbarClose = jQuery('.navbar-close'),
+        navBar = jQuery('.navigation');
+    if (navbarClose.length > 0) {
+        navbarClose.on('click', function () {
+            navBar.removeClass('push-menu-visible');
+            jQuery('body').removeClass('show-nav');
+            jQuery('.backdrop').remove();
+        });
+    }
+});
+
+//footer menu
+jQuery('.footer-toggle').click(function(){
+  jQuery(this).toggleClass('active');
+  jQuery(this).parent().find('.arrow').toggleClass('arrow-animate');
+  jQuery(this).parent().find('.content').slideToggle(280);
+});
