82bb463faa4285e641858492ee530901e51465b9
Arghyajit Lodh <arghyajit.lodh@deptagency.com>
2022-04-13 10:15:36 +0530

    BE changes for support form

--- web/modules/custom/tmg_utility/js/form_submission.js
+++ web/modules/custom/tmg_utility/js/form_submission.js
@@ -5,7 +5,18 @@
 
 (function ($, Drupal, drupalSettings) {
   'use strict';
+  Drupal.behaviors.webformRequiredErrorForClientSideValidation = {
+    attach: function (context) {
+      $(context).find(':input[data-webform-required-error]').once('webform-required-error').each(function() {
 
+        $(this).attr("required", "required");
+        var message = $(this).attr("data-webform-required-error");
+        if (message) {
+          $(this).attr("data-msg-required", message);
+        }
+      });
+    }
+  };
   Drupal.behaviors.auto_submit_form = {
     attach: function (context, settings) {
       $('.auto_submit_form').find('button[type="submit"]').trigger('click');
